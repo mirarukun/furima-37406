6fd3d5126526ef54782c7df9f758c0315143df24
mirarukun <miraru.395091@gmail.com>
2022-02-09 21:27:58 +0900

    マイグレーションファイルへテーブルの設計、モデルへのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :birthday, presence: true
+         validates :last_name_chinese, presence: true
+         validates :first_name_chinese, presence: true
+         validates :last_name_katakana, presence: true
+         validates :first_name_katakana, presence: true
+
 end
