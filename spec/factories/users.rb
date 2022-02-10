fbc3dc876a2731a246ffa0440ef2484e51c8114f
mirarukun <miraru.395091@gmail.com>
2022-02-10 18:09:00 +0900

    gimei追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     birthday              {'2000-01-01'}
     last_name_chinese     {'山田'}
