99dce1b4f1774f05da79ff6d1ca6f505cc9ebdcf
mirarukun <miraru.395091@gmail.com>
2022-02-10 21:33:53 +0900

    再修正：パスワードに英数字混合のバリデーションをかけた。また、それによるテストコードを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Lorem.characters(min_length: 6,min_alpha: 1, min_numric: 1)}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
     birthday              {'2000-01-01'}
     gimei = Gimei.name
