0d65ccf85d8c2f476cfcef3617d0a8737eb27fd6
mirarukun <miraru.395091@gmail.com>
2022-02-11 12:36:19 +0900

    'passwordは半角英数字混合でないと登録できない'をより詳しくテストできるように修正し、半角でのパスワード認証のテストを加えた。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,9 +44,23 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
 
-      it 'passwordは半角英数字混合でないと登録できない' do
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+
+      it '数字のみのパスワードでは登録できない' do
         @user.password = '123456'
         @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+      end
+
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'あいうえおか'
+        @user.password_confirmation = 'あいうえおか'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
       end
