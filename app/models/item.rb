bb57d17e90d878c02c1994111dff73b384ab0663
mirarukun <miraru.395091@gmail.com>
2022-02-11 17:46:38 +0900

    バリデーション設定した。アクティブハッシュ使う予定のものはnumericalityでとりあえず設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,19 @@
 class Item < ApplicationRecord
-belongs_to :user
-# has_one :purchase, dependent: :destroy   purchaseモデル作ったらコメントアウト外す予定
-has_one_attached :image
+  belongs_to :user
+  # has_one :purchase, dependent: :destroy   purchaseモデル作ったらコメントアウト外す予定
+  has_one_attached :image
+
+  with_options presence:true do
+    validates :item_name 
+    validates :item_text
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :status_id
+    validates :shipping_information_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
 end
