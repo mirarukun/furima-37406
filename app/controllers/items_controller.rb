e4dd06f867a711a5a86ed4552f6075315a050696
mirarukun <miraru.395091@gmail.com>
2022-02-12 20:27:37 +0900

    商品データが保存できるようにcreateアクションに記述、また保存できたかどうかで画面分岐するように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private 
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_text, :category_id, 
+      :status_id, :shipping_information_id, :prefecture_id, :shipping_day_id , :price).merge(user_id:current_user.id)
   end
 end
