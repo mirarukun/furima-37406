f93c49c74b698a5a53d911c3eb227f4ebebe5d5d
mirarukun <miraru.395091@gmail.com>
2022-02-12 22:09:20 +0900

    アクティブハッシュのプルダウンを表示できるようにビューファイル修正、また、モデルファイルに設定したクラス名アッパーキャメル型に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private 
 
   def item_params
-    params.require(:item).permit(:item_name, :item_text, :category_id, 
+    params.require(:item).permit(:image, :item_name, :item_text, :category_id, 
       :status_id, :shipping_information_id, :prefecture_id, :shipping_day_id , :price).merge(user_id:current_user.id)
   end
 end
