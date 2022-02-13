038c613ec46ddf6a3f4febb350cc95c2c575e884
mirarukun <miraru.395091@gmail.com>
2022-02-13 15:35:06 +0900

    使用する商品のテストデータ準備した

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {"オムレツ"}
+    item_text {Faker::Lorem.sentence}
+    category { Category.all.sample }
+    status { Status.all.sample }
+    shipping_information { ShippingInformation.all.sample }
+    prefecture { Prefecture.all.sample }
+    shipping_day { ShippingDay.all.sample }
+    price { 100000 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    association :user 
   end
 end
