9cca38aee31103d99282f5c2a32b753fe243622a
mirarukun <miraru.395091@gmail.com>
2022-02-13 17:37:03 +0900

    アクティブハッシュのモデルのid:0は選ばれないようにした

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     item_name {"オムレツ"}
     item_text {Faker::Lorem.sentence}
-    category { Category.all.sample }
-    status { Status.all.sample }
-    shipping_information { ShippingInformation.all.sample }
-    prefecture { Prefecture.all.sample }
-    shipping_day { ShippingDay.all.sample }
+    category { Category.where.not(id:0).sample }
+    status { Status.where.not(id:0).sample }
+    shipping_information { ShippingInformation.where.not(id:0).sample }
+    prefecture { Prefecture.where.not(id:0).sample }
+    shipping_day { ShippingDay.where.not(id:0).sample }
     price { 100000 }
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Item, type: :model do
   describe '商品出品' do
     context '商品が出品できる場合' do
       it '必要な情報を適切に入力して「出品する」ボタンを押すと、商品情報がデータベースに保存される' do
-
+        expect(@item).to be_valid
       end
       it '価格は、¥300~¥9,999,999の間であれば保存できる' do
 
