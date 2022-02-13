03f5865f357c4d8a75bcc8336df1b3aec4409586
mirarukun <miraru.395091@gmail.com>
2022-02-13 18:09:33 +0900

    itemモデルの単体テストコードを記述した

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,43 +11,71 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it '価格は、¥300~¥9,999,999の間であれば保存できる' do
-
+        @item.price = "300"
+        expect(@item).to be_valid
       end
     end
 
     context '商品が出品できない場合' do
-      it '商品画像を1枚つけないと保存できない' do
-
+      it '商品画像が空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品名が記入されていないと保存できない' do
-
+        @item.item_name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
       end
       it '商品の説明が記入されていないと保存できない' do
-
+        @item.item_text = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item text can't be blank")
       end
       it 'カテゴリーの情報が選択されていないと保存できない' do
-
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態の情報が選択されていないと保存できない' do
-
+        @item.status_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it '配送料の負担の情報が選択されていないと保存できない' do
-
+        @item.shipping_information_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping information can't be blank")
       end
       it '発送元の地域の情報が選択されていないと保存できない' do
-
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数の情報が選択されていないと保存できない' do
-
+        @item.shipping_day_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
       it '価格が記入されていないと保存できない' do
-
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格は、¥300~¥9,999,999の間でなければ保存できない' do
-
+      it '価格は、¥300より安いと保存できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
+      it '価格は、¥9,999,999より高いと保存できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
       it '価格は半角数値でなければ保存できない' do
-
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list", "Price is not a number")
       end
     end
   end
