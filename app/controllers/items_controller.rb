27ca869b32b0a276216c6627d7893e26f0be77d4
mirarukun <miraru.395091@gmail.com>
2022-02-14 23:33:11 +0900

    ログイン状態の場合でも、自身が出品していない商品の商品情報編集ページへ遷移しようとする(URL直接打ち込んで）と、商品の販売状況に関わらずトップページに遷移するように実装した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   before_action :move_to_signed_in, only: [:edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -46,6 +47,13 @@ class ItemsController < ApplicationController
   def move_to_signed_in
     unless user_signed_in?
       redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to action: :index
     end
   end
 end
