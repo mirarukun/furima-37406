bc3bf12bc84d94e3be83cb769ae1af02003ee31d
mirarukun <miraru.395091@gmail.com>
2022-02-15 13:08:58 +0900

    削除機能を実装、削除後トップページに遷移。またログインしていないユーザーや投稿者ではないユーザーは削除できないようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :edit]
-  before_action :assign_to_item_instance, only: [ :show, :edit, :update,]
-  before_action :move_to_index, only: [ :edit]
+  before_action :authenticate_user!, only: [ :new, :edit, :destroy]
+  before_action :assign_to_item_instance, only: [ :show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [ :edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,6 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.destroy
+      redirect_to action: :index
+    end
   end
 
   private
