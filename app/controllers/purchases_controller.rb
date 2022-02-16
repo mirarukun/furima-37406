32069c6d39614f977f94e7b48a7e01075a55ff8e
mirarukun <miraru.395091@gmail.com>
2022-02-16 21:25:20 +0900

    フォームの情報を保存できるようにコントローラーを編集した

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,5 +4,18 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+   else
+     render :index
+   end
+  end
+
+  private
+  
+  def purchase_params
+    params.require(:purchase_shipping).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id)
   end
 end
